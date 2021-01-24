56151c5e603901377872944a20696f2059ea215c
yiduohoulang2020 <yiduohoulang@163.com>
2021-01-25 00:07:34 +0800

    fix: order card

--- src/pages/bWithdrawal/index.jsx
+++ src/pages/bWithdrawal/index.jsx
@@ -5,6 +5,10 @@ import './index.scss'
 import BottomBtn from '@/components/bottomBtn'
 import WithdrawalCard from '@/components/withdrawalCard'
 import {AtInput} from 'taro-ui'
+import {commonHttpRequest} from '@/utils/servers/utils'
+import {useUserModel} from '@/models/user'
+import {createId} from '../../utils/idCreator'
+import {checkAndGetResult} from '../../utils/servers/utils'
 
 export default function () {
   const rate = '0.20'
@@ -18,6 +22,8 @@ export default function () {
   const [fee, setFee] = useState(0)
   const [loading, setLoading] = useState(false)
 
+  const {user} = useUserModel((model) => [model.user])
+
   useEffect(() => {
     setAmount({
       canWithdrawn: 23411.0,
@@ -25,6 +31,42 @@ export default function () {
       totalIncome: 30000.0,
     })
   }, [])
+
+  const withdrawal = () => {
+    if (inputVal <= 0) {
+      Taro.showToast({title: '请输入提现金额', icon: 'none'})
+      return
+    }
+    // TODO 验证提现金额大小
+
+    const id = createId()
+    const formData = {
+      id,
+      ownerId: user.id,
+      name: '申请提现',
+      amount: inputVal,
+      feeRate: rate,
+      feeAmount: fee,
+      enabled: true,
+    }
+    setLoading(true)
+    commonHttpRequest(
+      'withdrawalRecord',
+      'add',
+      {ownerId: user.id, id},
+      {},
+      formData,
+    ).then((res) => {
+      setLoading(false)
+      if (checkAndGetResult(res)) {
+        Taro.showToast({title: '成功发起提现申请', icon: 'none'})
+        setTimeout(() => {
+          // 跳转页面
+          Taro.navigateBack()
+        }, 600)
+      }
+    })
+  }
 
   useEffect(() => {
     const res = Math.round(inputVal * rate) / 100
@@ -35,14 +77,6 @@ export default function () {
     loading && Taro.showLoading({title: '正在提现...'})
     !loading && Taro.hideLoading()
   }, [loading])
-
-  const withdrawal = () => {
-    console.log('withdrawal')
-    setLoading(true)
-    setTimeout(() => {
-      setLoading(false)
-    }, 4000)
-  }
 
   useDidShow(() => {})
 
