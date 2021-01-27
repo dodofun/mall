31c45b8bcd95bd7b6fe5417f58b750abe2d67921
yiduohoulang2020 <yiduohoulang@163.com>
2021-01-27 17:32:02 +0800

    fix: bugs

--- src/app.config.js
+++ src/app.config.js
@@ -1,7 +1,7 @@
 export default {
   pages: [
-    'pages/bOrder/index', // 商户订单
     'pages/index/index', // 用户首页
+    'pages/bOrder/index', // 商户订单
     'pages/order/index', // 订单
     'pages/welfare/index', // 福利页
     'pages/bHome/index', // 商户主页
